0353a316a1ed635000bdf6b5262842f9955e2928
Thu Nguyen <thuminhnguyen@berkeley.edu>
2019-01-30 04:11:11 +0000

    Finished wc.c

--- hw0/wc.c
+++ hw0/wc.c
@@ -1,3 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdbool.h>
+
+void countLetters(FILE *input_file, char *file_name){
+	int lines_count = 0;
+	int words_count = 0;
+	int characters_count = 0;
+	bool is_whitespace = true;
+	char c;
+	char last_char = ' ';
+
+	// read file
+	while ((c = fgetc(input_file)) != EOF)  {
+		if (c == '\n' || c == '\0'){
+			lines_count += 1;
+			is_whitespace = true;
+		} else if (c == ' ' || c == '\t') {
+			is_whitespace = true;
+		}
+		// ecounter a character (not a whitespace)
+		else {
+			if (is_whitespace){
+				words_count++;
+			}
+			is_whitespace = false;
+		}
+		last_char = c;
+		characters_count++;
+	}
+	// If input is standard input
+	if (file_name == "stdin"){
+		printf (" %d   %d   %d\n", lines_count, words_count, characters_count);
+	} else{
+		printf (" %d  %d %d %s\n", lines_count, words_count, characters_count, file_name);	
+	}
+}
+
 int main(int argc, char *argv[]) {
+    if (argc == 2){
+    	FILE *file = fopen(argv[1], "r");
+    	char *file_name = argv[1];
+    	//if file open successfully
+	    if (file){
+			countLetters(file, file_name);
+	    } else{
+	    	printf("File %s failed to open.\n", file_name);
+	    }
+	    fclose(file);
+    } 
+    // No argument pass in. Get stdin input from keyboard
+    else if (argc == 1){
+    	// call count function with stdin as input file
+    	countLetters(stdin, "stdin");
+    }
     return 0;
 }
